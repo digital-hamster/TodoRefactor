a23b523de4f67060cd33bc647db45eb82d1a544e
digital-hamster <waterlove1439@naver.com>
2023-07-23 14:45:26 +0900

    눈엣가시 수정 . . .

---
+++ src/test/java/younah/TodoRefactor/TodoRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package younah.TodoRefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
