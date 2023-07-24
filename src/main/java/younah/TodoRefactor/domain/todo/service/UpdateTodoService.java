cf3a39b6ef3c727498ce1110806d603e09e47187
digital-hamster <waterlove1439@naver.com>
2023-07-24 17:43:12 +0900

    update 로직 수정

--- src/main/java/younah/TodoRefactor/domain/todo/service/UpdateTodoService.java
+++ src/main/java/younah/TodoRefactor/domain/todo/service/UpdateTodoService.java
@@ -17,12 +17,15 @@ public class UpdateTodoService {
     @Transactional
     public TodoDto update(long todoId, Requirement requirement){
 
+        Todo newTodo = requirement.toEntity();
+
         Todo targetTodo = todoRepo.findById(todoId)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.TODO_NOT_EXSIST));
 
-        targetTodo.update(requirement.content);
+        targetTodo.update(newTodo.getContent());
         //수정을 위해 받아온 requirement를 entity로 변환해야 하는가? ??
         // ㄴ> 음 ... 값만 받는데 그걸 requestBody로 받기 위해서 이런거임 ... 얘를 entity로 바꿀 필요는 없다 생각함
+            //ㄴ> 이래놓고 다시 수정 컨트롤러 서비스 침범 멈춰
         todoRepo.save(targetTodo);
 
         return TodoDto.fromEntity(targetTodo);
@@ -30,5 +33,9 @@ public class UpdateTodoService {
 
     public record Requirement(
             String content
-    ){ }
+    ){
+        public Todo toEntity(){
+        return new Todo(content);
+    }
+    }
 }
